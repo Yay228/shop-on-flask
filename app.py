dd5db6fb90bd11bc53f009d5194afec43db53d81
Teymur Mamedov <tima.mamedov.tima@mail.ru>
2023-06-09 22:26:54 +0300

    Added register and login

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template, request,redirect
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SECRET_KEY'] = '35af661f77853008b76aa25d99e42d8f'
 db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,42 +29,83 @@ class Post(db.Model):
     text = db.Column(db.Text, nullable=True)
     cost = db.Column(db.Integer, nullable=False)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
+@app.route('/home')
 @app.route('/')
 def hello_world():
     return render_template('main.html')
 
-
 @app.route('/shop')
+@login_required
 def shop():
     shop = Post.query.all()
     return render_template('shop.html', posts=shop)
 
-
-
-
 @app.route('/create', methods=['POST', 'GET'])
+@login_required
 def create():
     if request.method == "POST":
         title = request.form['title']
         text = request.form['text']
         cost = request.form['cost']
 
-
         post = Post(title=title, cost=cost, text=text)
- 
+
         try:
             db.session.add(post)
-            db.session.commit() 
+            db.session.commit()
             return redirect('/')
         except:
-            return "При добавлении произошла ошибка!"
-        
+            return "An error occurred while adding!"
+
     else:
         return render_template('create.html')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+        if user is not None and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('shop'))
+        else:
+            flash('Invalid username or password.')
+    return render_template('login.html')
+ 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            flash('Username already exists. Please choose another one.')
+            return redirect(url_for('register'))
+
+        new_user = User(username=username)
+        new_user.set_password(password)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('hello_world'))
 
 
 if __name__ == '__main__':
