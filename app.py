71093f8f96dddce3afd094f9fb65b69a147bb405
Teymur Mamedov <tima.mamedov.tima@mail.ru>
2023-06-10 11:12:11 +0300

    j

--- app.py
+++ app.py
@@ -2,15 +2,40 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+import re
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
 app.config['SECRET_KEY'] = '35af661f77853008b76aa25d99e42d8f'
 db = SQLAlchemy(app)
 
+
+
+migrate = Migrate(app, db)
+
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+
+def check_password_strength(password):
+    """
+    Проверяет сложность пароля.
+    Пароль должен быть как минимум 8 символов и содержать заглавные буквы, строчные буквы и цифры.
+    """
+    if len(password) < 8:
+        return False
+    elif not re.search("[a-z]", password):
+        return False
+    elif not re.search("[A-Z]", password):
+        return False
+    elif not re.search("[0-9]", password):
+        return False
+    else:
+        return True
+    
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -53,6 +78,11 @@ def create():
         text = request.form['text']
         cost = request.form['cost']
 
+        # проверяем, что поля title, text и cost не пустые
+        if not title or not text or not cost:
+            flash('Fields cannot be empty!')
+            return redirect(request.url)
+
         post = Post(title=title, cost=cost, text=text)
 
         try:
@@ -61,11 +91,10 @@ def create():
             return redirect('/')
         except:
             return "An error occurred while adding!"
-
     else:
         return render_template('create.html')
-
-
+    
+    
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -89,6 +118,10 @@ def register():
 
         if user:
             flash('Username already exists. Please choose another one.')
+            return redirect(url_for('register'))
+        
+        if not check_password_strength(password):
+            flash('Password is not strong enough. It should be at least 8 characters long, include uppercase, lowercase letters and numbers.')
             return redirect(url_for('register'))
 
         new_user = User(username=username)
